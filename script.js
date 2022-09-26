1f33dd380dda872d1c412de44d953a041aecb87f
Sagar <sagarbalamwar888@gmail.com>
2022-09-26 09:33:45 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgslider(anything) {
+  document.querySelector('.pepsi').src = anything;
+}
+
+function changebgcolor(color) {
+  const sec = document.querySelector('.sec');
+  sec.style.background = color;
+}
